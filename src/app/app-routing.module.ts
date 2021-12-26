02de6cb28c3114c3daac8b2b4c2af8dd43729dff
Bryan <bryanstevenal@gmail.com>
2021-12-26 23:09:03 +0100

    Add new components routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UsComponent } from "./components/us/us.component";
+import { OurServicesComponent } from "./components/our-services/our-services.component";
+import { ContactComponent } from "./components/contact/contact.component";
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: 'us', component: UsComponent },
+  { path: 'services', component: OurServicesComponent },
+  { path: 'contact', component: ContactComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
