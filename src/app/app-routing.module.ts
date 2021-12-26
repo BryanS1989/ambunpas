7eec4c53ef603f0e3dd4ea53b6621e45e7179c1c
Bryan <bryanstevenal@gmail.com>
2021-12-26 23:18:54 +0100

    Add routes for main component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+
+import { MainContentComponent } from "./components/main-content/main-content.component";
 import { UsComponent } from "./components/us/us.component";
 import { OurServicesComponent } from "./components/our-services/our-services.component";
 import { ContactComponent } from "./components/contact/contact.component";
@@ -8,7 +10,8 @@ import { ContactComponent } from "./components/contact/contact.component";
 const routes: Routes = [
   { path: 'us', component: UsComponent },
   { path: 'services', component: OurServicesComponent },
-  { path: 'contact', component: ContactComponent }
+  { path: 'contact', component: ContactComponent },
+  { path: '', component: MainContentComponent },
 ];
 
 @NgModule({
