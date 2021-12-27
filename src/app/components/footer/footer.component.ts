d08ecf4540bd775c55647e7b366139c6abbe9751
Bryan <bryanstevenal@gmail.com>
2021-12-27 20:30:28 +0100

    Add icons to footer

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,11 +1,26 @@
 import { Component, OnInit } from '@angular/core';
-
+import { faInstagram, faTwitter, faFacebook } from '@fortawesome/free-brands-svg-icons';
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent implements OnInit {
+
+  rrssArray = [
+    {
+      icon : faInstagram,
+      url : "https://instagram.com"
+    },
+    {
+      icon : faTwitter,
+      url : "https://twitter.com"
+    },
+    {
+      icon : faFacebook,
+      url : "https://facebook.com"
+    }
+  ];
 
   constructor() { }
 
