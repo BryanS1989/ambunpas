0ad728b5d1b6adf226f1c09f534c7c98695a1cfc
Bryan <bryanstevenal@gmail.com>
2021-12-27 21:16:55 +0100

    (WIP) Our service content page

--- src/app/components/our-services/our-services.component.ts
+++ src/app/components/our-services/our-services.component.ts
@@ -7,6 +7,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OurServicesComponent implements OnInit {
 
+  servicesArr = [
+    {
+      title : 'Cocinas',
+      subtitle : 'De construcción y a medida',
+      image : 'https://img77.uenicdn.com/image/upload/v1569615466/business/fb3d6536-fe92-4f56-aee2-3312168c51ea/IMG-1833JPG.jpg',
+      altImage : 'Photo of a Shiba Inu',
+      description :  'The Shiba Inu is the smallest of the six original and distinct spitz breeds of dog from Japan. A small, agile dog that copes very well with mountainous terrain, the Shiba Inu was originally bred for hunting.',
+      additionalLink : '',
+    },
+    {
+      title : 'Puertas',
+      subtitle : 'Madera, Metal...',
+      image : 'https://www.arquitecturayreforma.com/wp-content/uploads/Variedad-de-tipos-de-puertas.jpg',
+      altImage : 'Todas nuestras puertas',
+      description :  'The Shiba Inu is the smallest of the six original and distinct spitz breeds of dog from Japan. A small, agile dog that copes very well with mountainous terrain, the Shiba Inu was originally bred for hunting.',
+      additionalLink : '',
+    }
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
