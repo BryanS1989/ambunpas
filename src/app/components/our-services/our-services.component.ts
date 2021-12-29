e6a321ca77bd93d2e5ede1482d4a40b651241456
Bryan <bryanstevenal@gmail.com>
2021-12-29 21:38:57 +0100

    Add some cards to our-services component

--- src/app/components/our-services/our-services.component.ts
+++ src/app/components/our-services/our-services.component.ts
@@ -23,6 +23,36 @@ export class OurServicesComponent implements OnInit {
       altImage : 'Todas nuestras puertas',
       description :  'The Shiba Inu is the smallest of the six original and distinct spitz breeds of dog from Japan. A small, agile dog that copes very well with mountainous terrain, the Shiba Inu was originally bred for hunting.',
       additionalLink : '',
+    },
+    {
+      title : 'Armarios',
+      image : 'https://i.pinimg.com/originals/bf/90/84/bf9084c583d52510c76d789152bff0e0.jpg',
+      altImage : 'Todas nuestras puertas',
+      description :  'The Shiba Inu is the smallest of the six original and distinct spitz breeds of dog from Japan. A small, agile dog that copes very well with mountainous terrain, the Shiba Inu was originally bred for hunting.',
+      additionalLink : '',
+    },
+    {
+      title : 'Baños',
+      subtitle : 'Marmol, Cristal, Azulejos...',
+      image : 'https://www.coblonal.com/wp-content/uploads/2017/02/Hab-Andorra_048-1024x681.jpg',
+      altImage : 'Todas nuestras puertas',
+      description :  'The Shiba Inu is the smallest of the six original and distinct spitz breeds of dog from Japan. A small, agile dog that copes very well with mountainous terrain, the Shiba Inu was originally bred for hunting.',
+      additionalLink : '',
+    },
+    {
+      title : 'Tarima Flotante',
+      image : 'https://www.estratoshomes.com/wp-content/uploads/2020/06/portada-tarima-laminado-parquet.jpg',
+      altImage : 'Todas nuestras puertas',
+      description :  'The Shiba Inu is the smallest of the six original and distinct spitz breeds of dog from Japan. A small, agile dog that copes very well with mountainous terrain, the Shiba Inu was originally bred for hunting.',
+      additionalLink : '',
+    },
+    {
+      title : 'Fontanería Rápida',
+      subtitle : 'Madera, Metal...',
+      image : 'https://www.desatascosjumbo.com/wp-content/uploads/2021/02/www.emagister.com-cursoline-3602232-curso-online-de-fontaneria-materiales-para-instalaciones-fluxores-evacuacion-de-aguas-31.jpg',
+      altImage : 'Todas nuestras puertas',
+      description :  'The Shiba Inu is the smallest of the six original and distinct spitz breeds of dog from Japan. A small, agile dog that copes very well with mountainous terrain, the Shiba Inu was originally bred for hunting.',
+      additionalLink : '',
     }
   ];
 
