75e3f1b0cc70b9446c4849b753ab7de2a833d221
Bryan <bryanstevenal@gmail.com>
2021-12-30 19:36:59 +0100

    Responsive NavBar Component

--- src/app/components/nav-bar/nav-bar.component.ts
+++ src/app/components/nav-bar/nav-bar.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavBarComponent implements OnInit {
 
-  menu = [
+  menuArr = [
     {
       name : 'Nosotros',
       path : 'us'
